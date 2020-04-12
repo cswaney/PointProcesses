7609d1bfaecbc657da29b0787193d84ea0d5dc1c
Colin <colin.swaney@novacredit.com>
2020-04-12 08:40:51 -0400

    Update poisson.jl
    Tests for new Poisson processes.

--- test/poisson.jl
+++ test/poisson.jl
@@ -1,15 +1,21 @@
 using Distributions, Gadfly
 
-# Homogeneous
-p = PointProcess(1.)
-ts = rand(p)
+p = HomogeneousProcess(10., 1.)
+Ts = 10. * ones(100)
+ts = sort(vcat(map(T -> rand(p, T), Ts)...))
+plot(ts, Geom.histogram)
 
-# Inhomogeneous (Exponential)
-h = Exponential()
-p = PointProcess(1., h)
-ts = rand(p)
+# p = LinearProcess(10., 1.)
+# Ts = 10. * ones(100)
+# ts = sort(vcat(map(T -> rand(p, T), Ts)...))
+# plot(ts, Geom.histogram)
 
-# Inhomogeneous (Logistic-Normal)
-h = LogitNormal()
-p = PointProcess(1., h)
-ts = rand(p)
+p = ExponentialProcess(10., 1.)
+Ts = 10. * ones(100)
+ts = sort(vcat(map(T -> rand(p, T), Ts)...))
+plot(ts, Geom.histogram)
+
+p = LogitNormalProcess(10., 0., 1., 10.)
+Ts = 10. * ones(100)
+ts = sort(vcat(map(T -> rand(p, T), Ts)...))
+plot(ts, Geom.histogram)
