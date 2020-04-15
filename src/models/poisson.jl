a4ebff180de4b091f0239b9bf8b3909b87fb2c28
Colin <colin.swaney@novacredit.com>
2020-04-15 08:28:37 -0400

    Update poisson.jl
    Add function prototypes

--- src/models/poisson.jl
+++ src/models/poisson.jl
@@ -5,7 +5,21 @@ import Base.rand
 abstract type PoissonProcess end
 
 """
-rand(p::PoissonProcess, T::Float64)
+    intensity(p::PoissonProcess)
+
+Construct the intensity function for a point process, `t -> λ(t)`.
+"""
+function intensity(p::PoissonProcess) end
+
+"""
+    likelihood(p::PoissonProcess)
+
+Calculate the likelihood of a sequence of events over period `[0, T]`.
+"""
+function likelihood(p::PoissonProcess, events, T) end
+
+"""
+    rand(p::PoissonProcess, T::Float64)
 
 Construct a random sample from a point process `p` on interval [0, `T`].
 
@@ -22,11 +36,13 @@ struct HomogeneousProcess <: PoissonProcess
 end
 
 intensity(p::HomogeneousProcess) = t -> p.λ
+
 function likelihood(p::HomogeneousProcess, ts, T)
     a = exp(-p.λ * T)
     b = p.λ ^ length(ts)
     return a * b
 end
+
 function rand(p::HomogeneousProcess, T)
     n = rand(Poisson(p.λ * T))
     ts = rand(Uniform(0, T), n)
@@ -43,12 +59,14 @@ struct LinearProcess <: PoissonProcess
 end
 
 intensity(p::LinearProcess) = t -> p.b + p.w * t
+
 function likelihood(p::LinearProcess, ts, T)
     @warn "TODO: assert that linear intensity is non-negative on [0, T]."
     a = exp(-(0.5 * p.m * T ^ 2 + p.b * T))
     b = prod(p.m .* ts .+ p.b)
     return a * b
 end
+
 function rand(p::LinearProcess, T)
     @error "Method not implemented"
 end
@@ -63,18 +81,21 @@ struct ExponentialProcess <: PoissonProcess
 end
 
 intensity(p::ExponentialProcess) = t -> p.w * pdf(Exponential(p.θ), t)
+
 function likelihood(p::ExponentialProcess, ts, T)
     h = Exponential(p.θ)
     a = exp(-p.w * cdf(h, T))
     b = prod(p.w * pdf.(h, ts))
     return a * b
 end
+
 function rand(p::ExponentialProcess, T)
     h = Exponential(p.θ)
     n = rand(Poisson(p.w * cdf(h, T)))
     ts = rand(truncated(h, 0., T), n)
     return sort(ts)
 end
+
 
 """
 A Poisson process with LogitNormal distribution intensity function.
@@ -87,13 +108,16 @@ struct LogitNormalProcess <: PoissonProcess
 end
 
 intensity(p::LogitNormalProcess) = t -> p.w * pdf(LogitNormal(p.μ, p.τ), t / p.Δtmax)
+
 function likelihood(p::LogitNormalProcess, ts, T)
     d = LogitNormal(p.μ, p.τ)
     a = exp(-p.w * cdf(d, T / p.Δtmax))
     b = prod(p.w * pdf.(d, ts ./ p.Δtmax))
     return a * b
 end
+
 function rand(p::LogitNormalProcess, T)
+    T = max(p.Δtmax, T)
     h = LogitNormal(p.μ, p.τ)
     n = rand(Poisson(p.w * cdf(h, T / p.Δtmax)))
     ts = rand(truncated(h, 0., T / p.Δtmax), n)
