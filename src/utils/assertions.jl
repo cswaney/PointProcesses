ebaae86cc41c283d5b44433dd19a2a20c6ab7b26
Colin <colin.swaney@novacredit.com>
2020-04-20 06:17:41 -0400

    Update assertions.jl

--- src/utils/assertions.jl
+++ src/utils/assertions.jl
@@ -21,13 +21,3 @@ function assert_valid_intensity(h::Distribution)
         @error "Support of `h` must be non-negative"
     end
 end
-
-function assert_valid_intensity(h::UnivariateLinear)
-    supp = support(h)
-    if (supp.lb < 0)
-        @error "Support of `h` must be non-negative"
-    end
-    if !isnonnegative(h)
-        @error "Range of `h` must be non-negative"
-    end
-end
